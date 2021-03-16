1658c01a58650af860f18f77b0428af80d837938
Fenner314 <jacobgfenner@gmail.com>
2021-03-16 07:34:30 -0400

    finish html and css for nav, banner, and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,10 @@ import Copyright from './Copyright';
 
 export default function Contact() {
     return (
-        <div>
+        <div className="contact">
             <div className="contact-grid">
                 <div className="contact-grid-left">
-                    <img src={fullLogo} width="250px" alt="store" />
+                    <img src={fullLogo} width="250px" alt="store" style={{marginTop: '-4.5rem', marginBottom: '-4.5rem'}}/>
                     <p>Privacy Policy</p>
                     <p>Refund Policy</p>
                     <p>Sizing Chart</p>
@@ -17,13 +17,20 @@ export default function Contact() {
                     <p>Aberdeen, SD 57401</p>
                     <p>(605) 380-3352</p>
                 </div>
-                <div className="contact-grid-right_form">
-                    <input type="text" placeholder="Name" name="name" aria-label="name" />
+                <div className="contact-grid-right">
+                    <div></div>
+                    <div className="contact-grid-form">
+                        <input className="contact-form-input" type="text" placeholder="Name" name="name" aria-label="name" />
+                        <input className="contact-form-input" type="text" placeholder="Email Address" name="email address" aria-label="email address" />
+                        <input className="contact-form-input" type="text" placeholder="Subject" name="subject" aria-label="subject" />
+                        <textarea className="contact-form-input" id="message" placeholder="Message" name="message" aria-label="message" />
+                    </div>
                 </div>
             </div>
             <div className="newsletter">
-                <h3>Sign up for newsletter</h3>
+                <h5>Sign up for newsletter</h5>
                 <input id="newsletter" type="email" placeholder="email@example.com" name="email" aria-label="email@example.com" autoCorrect="off" />
+                <button>Subscribe</button>
             </div>
             <Copyright /> 
         </div>
